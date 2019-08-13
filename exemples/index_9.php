587ac81a348e5444824c8d2eb70be917edd26516
GC7 <GrCOTE7@Gmail.com>
2019-08-13 19:11:32 +0200

    Card

---
+++ exemples/index_9.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <link rel="icon" href="../../../../favicon.ico" />
+    <title>Starter Template for Bootstrap</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+    <link href="css/tuto7.css" rel="stylesheet" />
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+    <script src="https://use.fontawesome.com/0b5fa10a1b.js"></script>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="header">
+            <h1>Card</h1>
+        </div>
+        <div class="card mt-2">
+            <p class="card-text">Un peu de texte pour montrer comment il se comporte à l'intérieur d'une carte.</p>
+
+            <div class="card-body">
+                <img src="../img/t12.jpg"><span><em> Une image</em></span>
+                <p class="card-text">Un peu de texte justifié à gauche</p>
+                <p class="card-text text-right">Un peu de texte justifié à droite</p>
+                <p class="card-text text-center">Un peu de texte centré</p>
+                <p><a href="#" class="card-link">Un lien</a></p>
+                <button class="btn btn-primary">Un bouton à gauche</button>
+                <button class="btn btn-primary float-right">Un bouton à droite</button>
+            </div>
+        </div>
+    </div>
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+
+
+</body>
+
+</html>
